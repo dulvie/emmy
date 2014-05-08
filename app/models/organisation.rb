cc0b94983bfcec698db8b4af2aef56a717aba36e
joel hansson <joel.hansson@gmail.com>
2014-05-08 21:55:18 +0200

    add some fields to organisation model

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -1,19 +1,26 @@
+# @todo Change this into dynamic and updateable content
 class Organisation
 
   def self.name
+    "Kinal"
   end
-
 
   def self.address
-  end
-
-  def self.vat
   end
 
   def self.zip
   end
 
+  def self.vat_number
+  end
+
   def self.bankgiro
+  end
+
+  def self.postgiro
+  end
+
+  def self.plusgiro
   end
 
 end
