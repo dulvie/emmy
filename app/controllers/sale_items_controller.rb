ff00f49d033b854460b3929a6211fbe191c83fff
joel hansson <joel.hansson@gmail.com>
2014-05-19 16:47:39 +0200

    make new sale_item a separate page

--- app/controllers/sale_items_controller.rb
+++ app/controllers/sale_items_controller.rb
@@ -1,16 +1,21 @@
 class SaleItemsController < ApplicationController
-  load_and_authorize_resource
+  load_and_authorize_resource :sale
+  load_and_authorize_resource :sale_item, through: :sale
+
+  def new
+    gon.push sale: @sale, sale_item: @sale_item
+  end
 
   def create
     @sale = Sale.find(params[:sale_id])
     @sale_item = @sale.sale_items.build sale_item_params
-    if @sale_item.save
-      msg_prms = {notice: "#{t(:product_added)}"}
-    else
-      msg_prms = {notice: "#{t(:failed_to_add_product)}"}
-    end
     respond_to do |format|
-      format.html { redirect_to edit_sale_path(@sale), msg_prms }
+      if @sale_item.save
+        format.html { redirect_to edit_sale_path(@sale), notice: "#{t(:product_added)}" }
+      else
+        flash.now[:danger] = "#{t(:failed_to_add)} #{t(:product)}"
+        format.html { render :new }
+      end
     end
   end
 
