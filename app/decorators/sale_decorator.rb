26176fa02a4f06d0eef78a479e5d0f1acc0739c2
joel hansson <joel.hansson@gmail.com>
2014-05-19 17:41:19 +0200

    adds color to state on sales index

--- app/decorators/sale_decorator.rb
+++ app/decorators/sale_decorator.rb
@@ -2,24 +2,51 @@ class SaleDecorator < Draper::Decorator
   delegate_all
 
   def states
-    [object.state, pretty_goods_state, pretty_money_state].join(" / ")
+    [pretty_state, pretty_goods_state, pretty_money_state]
+  end
+
+  def pretty_state
+    l = 'default'
+    case object.state
+    when 'complete'
+      l = 'success'
+      str = 'complete'
+    when 'incomplete'
+      l = 'warning'
+      str = 'incomplete'
+    end
+    labelify(str, l)
   end
 
   def pretty_goods_state
+    l = 'default'
     case object.goods_state
     when 'not_delivered'
-      h.t(:not_delivered)
+      l = 'warning'
+      str = h.t(:not_delivered)
     when 'delivered'
-      h.t(:delivered)
+      l = 'success'
+      str = h.t(:delivered)
     end
+    labelify(str, l)
   end
 
   def pretty_money_state
+    l = 'default'
     case object.money_state
     when 'not_paid'
-      h.t(:not_paid)
+      l = 'danger'
+      str = h.t(:not_paid)
     when 'paid'
-      h.t(:paid)
+      l = 'success'
+      str = h.t(:paid)
+    end
+    labelify(str, l)
+  end
+
+  def labelify(str, label_state)
+    h.content_tag :span, class: "label label-#{label_state}" do
+      str
     end
   end
 
