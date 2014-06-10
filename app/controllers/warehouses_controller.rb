0df457083e24d6fee0b5838aa1fc13d387ea7924
mhn <max.hansson@gmail.com>
2014-06-10 19:51:58 +0200

    Fix warehouse redirect

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -33,7 +33,7 @@ class WarehousesController < ApplicationController
 
     respond_to do |format|
       if @warehouse.save
-        format.html { redirect_to @warehouse, notice: 'Warehouse was successfully created.' }
+        format.html { redirect_to  warehouses_path, notice: 'Warehouse was successfully created.' }
         # format.json { render action: 'show', status: :created, location: @warehouse }
       else
         format.html { render action: 'new' }
@@ -47,7 +47,7 @@ class WarehousesController < ApplicationController
   def update
     respond_to do |format|
       if @warehouse.update(warehouse_params)
-        format.html { redirect_to @warehouse, notice: 'Warehouse was successfully updated.' }
+        format.html { redirect_to  warehouses_path, notice: 'Warehouse was successfully updated.' }
         # format.json { head :no_content }
       else
         format.html { render action: 'edit' }
