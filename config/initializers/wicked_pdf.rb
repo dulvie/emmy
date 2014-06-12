781fa2219aa927bc0fac0ee6bd8d70c3120e18e0
joel hansson <joel.hansson@gmail.com>
2014-06-12 12:07:50 +0200

    use dev wkhtmltopdf in prod aswell

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,9 +1,6 @@
-Mime::Type.register "application/pdf", :pdf
-if Rails.env.test? or Rails.env.development?
-  WickedPdf.config = {
-    wkhtmltopdf: Rails.root.join('bin', 'wkhtmltopdf-local').to_s,
-    exe_path: "bundle exec #{Rails.root.join('bin', 'wkhtmltopdf-local')}",
-  }
-else
-  raise RuntimeException, "production need special care for wkhtmltopdf"
-end
+# PDF mimetype is already specified in action_dispatch
+# Mime::Type.register "application/pdf", :pdf
+WickedPdf.config = {
+  wkhtmltopdf: Rails.root.join('bin', 'wkhtmltopdf-local').to_s,
+  exe_path: "bundle exec #{Rails.root.join('bin', 'wkhtmltopdf-local')}",
+}
