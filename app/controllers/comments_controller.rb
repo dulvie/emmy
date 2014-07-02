4e8a9d7a0e91571c551c0506b13ca539cc9b8214
mhn <max.hansson@gmail.com>
2014-07-02 12:48:42 +0200

    Added comment without parent

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,12 +6,18 @@ class CommentsController < ApplicationController
   before_filter :show_breadcrumbs, only: [:show, :update]
   before_filter :new_breadcrumbs, only: [:new, :create]
 
+  def index
+    @comments = Comment.where('parent_id= ?', 0)
+  end
+
   def new
-    @comment = @parent.comments.build
-    logger.info "comment parent: #{@comment.parent_type}"
-    logger.info "comment: #{@comment.inspect}"
-    logger.info "parent: #{@parent.inspect}"
-    @comment_form_url = comments_path(parent_type: @comment.parent_type, parent_id: @comment.parent_id)
+    if @parent.nil?
+      @comment = Comment.new
+      @comment_form_url = comments_path(parent_type: 'nil', parent_id: 0)
+    else
+      @comment = @parent.comments.build
+      @comment_form_url = comments_path(parent_type: @comment.parent_type, parent_id: @comment.parent_id)
+    end   
   end
 
   def show
@@ -19,10 +25,19 @@ class CommentsController < ApplicationController
   end
 
   def create
-    @comment = @parent.comments.build comment_params
+    if @parent.nil?
+      @comment = Comment.create comment_params
+      @comment.parent_type = 'nil'
+      @comment.parent_id = 0
+      redirect_path = comments_path
+    else
+      @comment = @parent.comments.build comment_params
+      redirect_path = edit_polymorphic_path(@parent)
+    end
+    @comment.user = current_user
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to edit_polymorphic_path(@parent), notice: "#{t(:comment_added)}" }
+        format.html { redirect_to redirect_path, notice: "#{t(:comment_added)}" }
       else
         flash.now[:danger] = "#{t(:failed_to_add)} #{t(:comment)}"
         format.html { render :new }
@@ -31,9 +46,14 @@ class CommentsController < ApplicationController
   end
 
   def update
+    if @parent.nil?
+      redirect_path = comments_path
+    else
+      redirect_path = edit_polymorphic_path(@parent)
+    end
     respond_to do |format|
       if @comment.update(comment_params)
-        format.html { redirect_to edit_polymorphic_path(@parent), notice: "#{t(:comment)} #{t(:was_successfully_updated)}" }
+        format.html { redirect_to redirect_path, notice: "#{t(:comment)} #{t(:was_successfully_updated)}" }
       else
         flash.now[:danger] = "#{t(:failed_to_update)} #{t(:comment)}"
         format.html { render :show }
@@ -42,9 +62,14 @@ class CommentsController < ApplicationController
   end
 
   def destroy
+    if @parent.nil?
+      redirect_path = comments_path
+    else
+      redirect_path = edit_polymorphic_path(@parent)
+    end
     @comment.destroy
     respond_to do |format|
-      format.html { redirect_to edit_polymorphic_path(@parent), notice: "#{t(:comment)} #{t(:was_destroyed)}" }
+      format.html { redirect_to redirect_path, notice: "#{t(:comment)} #{t(:was_destroyed)}" }
       #format.json { head :no_comment }
     end
   end
@@ -55,9 +80,15 @@ class CommentsController < ApplicationController
     def find_and_authorize_parent
 
       # if comment exists, but no parent_type, add from comment.
-      unless params.has_key?(:parent_type) && @comment
+      if !params.has_key?(:parent_type) && !@comment.nil?
         params[:parent_type] = @comment.parent_type
         params[:parent_id] = @comment.parent_id
+      end
+
+      logger.info "Kollar:: #{params[:parent_type]}"
+      if  !params.has_key?(:parent_type)
+         params[:parent_type] = 'nil'
+         params[:parent_id] = 0
       end
 
       unless Comment::VALID_PARENT_TYPES.include? params[:parent_type]
@@ -65,10 +96,12 @@ class CommentsController < ApplicationController
         redirect_to '/'
         return false
       end
-
-      parent_class = params[:parent_type].constantize
-      @parent = parent_class.find(params[:parent_id])
-      authorize! :manage, @parent
+      
+      if params[:parent_type] != 'nil'
+        parent_class = params[:parent_type].constantize
+        @parent = parent_class.find(params[:parent_id])
+        authorize! :manage, @parent
+      end  
     end
 
     def comment_params
@@ -76,13 +109,17 @@ class CommentsController < ApplicationController
     end
 
     def show_breadcrumbs
-      @breadcrumbs = [["#{@comment.parent_type.pluralize}", send("#{@parent.class.name.downcase}s_path")], 
-      [@comment.parent_name, @comment.parent], ["#{t(:comments)}(#{@comment.body})"]]
+      if !@parent.nil?  
+        @breadcrumbs = [["#{@comment.parent_type.pluralize}", send("#{@parent.class.name.downcase}s_path")], 
+        [@comment.parent_name, @comment.parent], ["#{t(:comments)}(#{@comment.body})"]]
+      end  
     end
 
     def new_breadcrumbs
-      @breadcrumbs = [["#{@parent.class.name.pluralize}", send("#{@parent.class.name.downcase}s_path")], 
-      [@parent.parent_name, @parent], ["#{t(:new)} #{t(:comment)}"]]
+      if !@parent.nil?
+        @breadcrumbs = [["#{@parent.class.name.pluralize}", send("#{@parent.class.name.downcase}s_path")], 
+        [@parent.parent_name, @parent], ["#{t(:new)} #{t(:comment)}"]]
+      end  
     end
 
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -9,7 +9,7 @@ class Comment < ActiveRecord::Base
 
   attr_accessible :body, :user, :parent, :user_id, :parent_id, :parent_type
 
-  VALID_PARENT_TYPES = ['Customer', 'Supplier', 'Warehouse', 'Manual', 'Transfer', 'Import', 'Production']
+  VALID_PARENT_TYPES = ['Customer', 'Supplier', 'Warehouse', 'Manual', 'Transfer', 'Import', 'Production', 'nil']
 
   # For ApplicationHelper#delete_button
   def can_delete?
