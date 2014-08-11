3b702b6f5de8d06b4290ba737cf742423ccc013d
joel hansson <joel.hansson@gmail.com>
2014-08-11 20:47:18 +0200

    wkhtmltopdf tweaks

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -4,3 +4,6 @@ WickedPdf.config = {
   wkhtmltopdf: Rails.root.join('bin', 'wkhtmltopdf-local').to_s,
   exe_path: "bundle exec #{Rails.root.join('bin', 'wkhtmltopdf-local')}",
 }
+
+puts "wkhtmltopdf path: #{WickedPdf.config[:wkhtmltopdf]}"
+raise RuntimeError, "cant find wkhtmltopdf" unless File.exists?("#{WickedPdf.config[:wkhtmltopdf]}")
