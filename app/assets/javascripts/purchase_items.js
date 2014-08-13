c391029bd7b04afeac6141f6a7b294148e3a1dd8
mhn <max.hansson@gmail.com>
2014-08-13 09:46:57 +0200

    Purchase_item, don't show batch if not sock-item close #36

--- app/assets/javascripts/purchase_items.js
+++ app/assets/javascripts/purchase_items.js
@@ -1,18 +1,21 @@
 app.controller('purchase_items_form_ctrl', function ($scope) {
-
+	$scope.show_batch = false;
     var o = $("#purchase_item_item_id").val();
 	//var o =  $("#purchase_item_item_id").find('option').first().val();
 	$scope.item_id = o;
 
 	$scope.item_changed = function() {
+		$scope.show_batch = false;
 		$scope.batch = {};
 		for (var x=0; x < gon.items.length; x++) {
 			if ($scope.item_id == gon.items[x].id) {
 				$scope.options = gon.items[x].batches;
 				$scope.unit = gon.items[x].unit.name;
 				$('#purchase_item_price').val(gon.items[x].in_price);
-				if (typeof($scope.options[0]) != "undefined")
+				if (typeof($scope.options[0]) != "undefined") {
 					$scope.batch.id = $scope.options[0].id;
+					$scope.show_batch = true;
+				}
 			}
 		}
 	}
