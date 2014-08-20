51dd6531646195836ff38ad476f0690862fed0d6
joel hansson <joel.hansson@gmail.com>
2014-08-20 13:03:26 +0200

    remove paperclip docs when running reload_db.sh; fixes #1

--- reload_db.sh
+++ reload_db.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/bash -x
 db_name=$1
 
 if [ "$db_name" == "" ]; then
   db_name='emmy_development'
 fi
 be='bundle exec'
-dropdb $db_name && $be rake db:create && $be rake db:migrate && $be rake db:seed
+dropdb $db_name && $be rake db:create && $be rake db:migrate && $be rake db:seed && rm public/system/documents
