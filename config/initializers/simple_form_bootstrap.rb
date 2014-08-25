6888cdd7e34119814eeb0aa42d216bbc31183d29
joel hansson <joel.hansson@gmail.com>
2014-08-25 12:24:50 +0200

    widen the comments text area

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -97,6 +97,18 @@ SimpleForm.setup do |config|
     end
   end
 
+  config.wrappers :horizontal_text_area, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
+    b.use :html5
+    b.use :placeholder
+    b.use :label, class: 'col-sm-11 col-md-9 col-lg-7 control-label'
+
+    b.wrapper tag: 'div', class: 'col-sm-30 col-md-30 col-lg-20' do |ba|
+      ba.use :input, class: 'form-control'
+      ba.use :error, wrap_with: { tag: 'span', class: 'help-block' }
+      ba.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
+    end
+  end
+
   config.wrappers :bare, tag: false, class: 'form-group', error_class: 'has-error' do |b|
     b.use :html5
     b.use :placeholder
