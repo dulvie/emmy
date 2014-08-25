bdddea905920fe1bebbdfda722ebf3d76c35b09d
joel hansson <joel.hansson@gmail.com>
2014-08-25 10:02:05 +0200

    put breadcrumbs definition in the top of the action method

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,10 +1,10 @@
 class DashboardController < ApplicationController
 
   def index
+    @breadcrumbs = [['Dashboard']]
     @current_organisation = current_organisation
     authorize! :read, current_user
     @todo_content = File.read(Rails.root + "TODO")
-    @breadcrumbs = [['Dashboard']]
   end
 
 end
