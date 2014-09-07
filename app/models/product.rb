d80abfd2f85f27dee42a57c0f6f35653e047fc4a
joel hansson <joel.hansson@gmail.com>
2014-09-07 17:43:17 +0200

    rubocop fixes for product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,11 @@
 class Product
   attr_accessor :value, :name, :available_quantity, :stocked,
-    :distributor_price, :retail_price, :refined_at, :expire_at,
-    :selected
+                :distributor_price, :retail_price, :refined_at, :expire_at,
+                :selected
 
   def initialize(h = {})
-   setup_defaults
-    h.each {|k,v| instance_variable_set("@#{k}",v)}
+    setup_defaults
+    h.each { |k, v| instance_variable_set("@#{k}", v) }
   end
 
   def setup_defaults
@@ -16,8 +16,8 @@ class Product
   end
 
   def self.find_using_shelves(shelves)
-    products = shelves.collect{|s| s.to_product }
-    non_shelf_items = Item.sellable.not_stocked.collect{|i| i.to_product }
+    products = shelves.map { |s| s.to_product }
+    non_shelf_items = Item.sellable.not_stocked.map { |i| i.to_product }
     products += non_shelf_items
     products
   end
