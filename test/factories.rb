6ebca61edaa66d3ec3364b970e66caa64ab386ef
mhn <max.hansson@gmail.com>
2014-09-29 14:33:01 +0200

    test

--- test/factories.rb
+++ test/factories.rb
@@ -18,24 +18,30 @@ FactoryGirl.define do
     name :seller
   end
 
-  factory :batch do
-    name "espresso 2014"
-    in_price 10000
-    distributor_price 15000
-    retail_price 20000
-  end
-
   factory :unit do
     name 'kg'
+  end
+
+  factory :vat do
+    name '12 procent'
+    vat_percent 12
   end
 
   factory :item do
     name 'espresso'
     unit Unit.all.first || FactoryGirl.create(:unit)
+    vat Vat.all.first || FactoryGirl.create(:vat)
     item_type 'sales'
     item_group 'refined'
   end
 
+  factory :batch do
+    name "espresso 2014"
+    in_price 10000
+    distributor_price 15000
+    retail_price 20000
+    item Item.all.first || FactoryGirl.create(:item)
+  end
 
   factory :warehouse do
     name "Coffee place"
@@ -46,6 +52,7 @@ FactoryGirl.define do
 
   factory :customer do
     name "CoffeeHouse foobar"
+    payment_term 30
   end
 
   factory :supplier do
