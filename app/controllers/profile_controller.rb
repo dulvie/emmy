4a445a083ab5e118261c5f19992409324505eeea
mhn <max.hansson@gmail.com>
2014-10-02 09:57:28 +0200

    error in profile

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -6,7 +6,7 @@ class ProfileController < ApplicationController
   def update
     @user = current_user
     if @user.update(user_params)
-      flash.now[:notice] = "#{t(:your_profile)} #{t(:was_successfully_updated}"
+      flash.now[:notice] = "#{t(:your_profile)} #{t(:was_successfully_updated)}"
     else
       flash.now[:error] = "#{t(:failed_to_update)} #{t(:your_profile)}."
     end
