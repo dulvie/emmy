b19ba716fdaac3718ffc4c4060ca1a7516a97e01
mhn <max.hansson@gmail.com>
2014-10-08 15:20:22 +0200

    price handling batch

--- app/assets/javascripts/batches.js
+++ app/assets/javascripts/batches.js
@@ -5,8 +5,7 @@ app.controller('batch_form_ctrl', function ($scope, price) {
 		$scope.sales = true;
 		$scope.item_id = $('#batch_item_id').val();
 		var default_price = false;
-		if ($('#batch_distributor_price').val()=='')
-			default_price = true;
+
 		var ex = $('#in_expire_at').val().split(/\D/);
 		ex.length == 1 ? $scope.ex_date = new Date() : $scope.ex_date = new Date(ex[0], --ex[1], ex[2]);
 
@@ -35,14 +34,13 @@ app.controller('batch_form_ctrl', function ($scope, price) {
 		$scope.sales = true;
 		for (x=0; x < gon.items.length; x++) {
 			if (gon.items[x].id == $scope.item_id) {
-
-				$('#batch_in_price').val(price.toDecimal(gon.items[x].in_price));
-				if (default_price)
+				if (default_price) {
 					$('#batch_distributor_price').val(gon.items[x].distributor_price);
-				if (default_price)
 					$('#batch_retail_price').val(gon.items[x].retail_price);
+				}
+				$('#batch_in_price').val(price.toDecimal($('#batch_in_price').val()));
 				$('#batch_distributor_price').val(price.toDecimal($('#batch_distributor_price').val()));
-			    $('#batch_retail_price').val(price.toDecimal($('#batch_retail_price').val()));			    	
+				$('#batch_retail_price').val(price.toDecimal($('#batch_retail_price').val()));			    	
 				if (gon.items[x].item_group == 'refined') {
 					$scope.refined = false;
 				}
