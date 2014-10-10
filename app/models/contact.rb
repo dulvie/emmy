90fb37aeca3ecba233938159c3f040a879f35778
mhn <max.hansson@gmail.com>
2014-10-10 11:31:56 +0200

    contats validation test not needed ref91, 06562fdeccef220cb1f7473d707b78fbed9a5337

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -15,19 +15,10 @@ class Contact < ActiveRecord::Base
   attr_accessible :email, :name, :telephone, :address, :zip, :city, :country, :comment, :organization
 
   validates :name, presence: true
-  validates :email, uniqueness: true, if: :check_email
+  validates :email, uniqueness: true
   validates :email, presence: true
 
   VALID_PARENT_TYPES = ['Customer', 'Supplier', 'Warehouse', 'ContactRelation', 'User']
-
-  def check_email
-    if new_record?
-      return false if organization.contacts.where('email=?', email).count == 0
-    else
-      return false if organization.contacts.where('email=? and id=?', email, id).count == 0
-    end
-    true
-  end
 
   # For ApplicationHelper#delete_button
   def can_delete?
