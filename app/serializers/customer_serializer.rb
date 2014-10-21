4ef5f63f4d32a1c6f3f8a8fa77686e1966160c6d
mhn <max.hansson@gmail.com>
2014-10-21 08:09:37 +0200

    sale - select primary contact, ref #102

--- app/serializers/customer_serializer.rb
+++ app/serializers/customer_serializer.rb
@@ -1,5 +1,4 @@
 class CustomerSerializer < ActiveModel::Serializer
-  attributes :id, :name, :vat_number, :address, :zip, :city, :payment_term
-
+  attributes :id, :name, :vat_number, :address, :zip, :city, :payment_term, :primary_contact_id
   has_many :contacts
 end
