55be6c62fce58118e67508b26c5067f43682d27d
mhn <max.hansson@gmail.com>
2014-10-21 21:10:46 +0200

    price-handling purchase_item, ref #114

--- app/assets/javascripts/purchase_items.js
+++ app/assets/javascripts/purchase_items.js
@@ -1,17 +1,16 @@
 app.controller('purchase_items_form_ctrl', function ($scope, $filter, price) {
 	
 	$scope.show_batch = false;
-    var o = $("#purchase_item_item_id").val();
+  var o = $("#purchase_item_item_id").val();
 	$scope.item_id = o;
 	var p = $('#purchase_item_price').val();
 	var pri = true;
-    if (p == '') {
-    	pri = false;
-    }
-    $scope.before_submit = function() {
-    	var p = $('#purchase_item_price').val();
-    	$('#purchase_item_price').val(price.toInteger(p));
-    };
+  if (p == '') {
+  	pri = false;
+  }
+  $scope.before_submit = function() {
+   	$('#purchase_item_price').val(price.toInteger($('#price_edit').val()));
+  };
 
 	$scope.item_changed = function() {
 		$scope.show_batch = false;
@@ -20,17 +19,14 @@ app.controller('purchase_items_form_ctrl', function ($scope, $filter, price) {
 			if ($scope.item_id == gon.items[x].id) {
 				$scope.options = gon.items[x].batches;
 				$scope.unit = gon.items[x].unit.name;
-				//$('.money').unmask();
 				if (pri) {
-					$('#purchase_item_price').val(price.toDecimal(p));
+					$('#price_edit').val(price.toDecimal(p));
 					pri = false;
 				}	
 				else {
 					var pr = gon.items[x].in_price;
-					$('#purchase_item_price').val(price.toDecimal(pr));
+					$('#price_edit').val(price.toDecimal(pr));
 				}
-				//$('.money').mask('00009.00', {reverse: true});
-				//$('#purchase_item_price').val(gon.items[x].in_price);
 				if (typeof($scope.options[0]) != "undefined") {
 					$scope.batch.id = $scope.options[0].id;
 					$scope.show_batch = true;
