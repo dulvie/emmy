244b618acdd66be78d09ccd8863deb70fe47ff09
mhn <max.hansson@gmail.com>
2014-10-21 20:54:57 +0200

    price-handling item, ref #114

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,12 +1,12 @@
 app.controller('item_form_ctrl', function ($scope, price) {
 	$scope.init = function() {
-		$('#item_in_price').val(price.toDecimal($('#item_in_price').val()));
-    	$('#item_distributor_price').val(price.toDecimal($('#item_distributor_price').val()));
-    	$('#item_retail_price').val(price.toDecimal($('#item_retail_price').val()));
+		$('#in_price_edit').val(price.toDecimal($('#item_in_price').val()));
+    $('#distributor_price_edit').val(price.toDecimal($('#item_distributor_price').val()));
+    $('#retail_price_edit').val(price.toDecimal($('#item_retail_price').val()));
 	};
 	$scope.before_submit = function() {
-    	$('#item_in_price').val(price.toInteger($('#item_in_price').val()));
-    	$('#item_distributor_price').val(price.toInteger($('#item_distributor_price').val()));
-    	$('#item_retail_price').val(price.toInteger($('#item_retail_price').val()));
+    $('#item_in_price').val(price.toInteger($('#in_price_edit').val()));
+    $('#item_distributor_price').val(price.toInteger($('#distributor_price_edit').val()));
+    $('#item_retail_price').val(price.toInteger($('#retail_price_edit').val()));
 	};	
 });
