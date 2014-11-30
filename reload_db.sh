e95aa381e3cb4bc1f9901c407d8ef87c9c576273
joel hansson <joel.hansson@gmail.com>
2014-11-30 17:12:38 +0100

    use foreman run to rebuild database

--- reload_db.sh
+++ reload_db.sh
@@ -4,5 +4,6 @@ db_name=$1
 if [ "$db_name" == "" ]; then
   db_name='emmy_development'
 fi
-be='bundle exec'
-dropdb $db_name && $be rake db:create && $be rake db:migrate && $be rake db:seed && rm public/system/documents
+#be='bundle exec'
+be='foreman run'
+sudo -u postgres dropdb $db_name && sudo -u postgres createdb $db_name && $be rake db:create && $be rake db:migrate && $be rake db:seed && rm public/system/documents
