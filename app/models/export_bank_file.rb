46c5ac76d098a8bbd3f4a89e94c6a839d3f6adde
mhn <max.hansson@gmail.com>
2015-05-22 15:05:41 +0200

    export bank file preliminary functions

--- app/models/export_bank_file.rb
+++ app/models/export_bank_file.rb
@@ -4,12 +4,16 @@ class ExportBankFile < ActiveRecord::Base
   # t.datetime :from_date
   # t.datetime :to_date
   # t.string   :reference
+  # t.string   :pay_account
+  # t.string   :organization_number
+  # t.string   :iban
   # t.integer  :organization_id
   # t.timestamps
 
-  attr_accessible :export_date, :from_date, :to_date, :reference
+  attr_accessible :export_date, :from_date, :to_date, :reference, :pay_account, :organization_number, :iban
 
   belongs_to :organization
+  has_many   :export_bank_file_rows, dependent: :delete_all
   # validates_attachment_content_type :upload, content_type: ['text/csv']
 
   def can_delete?
