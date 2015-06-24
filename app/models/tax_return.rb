e8e13e01f08323d6dc3faf8734973a73ec88443f
mhn <max.hansson@gmail.com>
2015-06-24 13:00:36 +0200

    move tax_return_report to job

--- app/models/tax_return.rb
+++ app/models/tax_return.rb
@@ -22,13 +22,14 @@ class TaxReturn < ActiveRecord::Base
 
   STATE_CHANGES = [:mark_calculated, :mark_reported]
 
-  def state_change(event, changed_at = nil)
+  def state_change(event, changed_at = nil, user_id = nil)
     return false unless STATE_CHANGES.include?(event.to_sym)
-    send(event, changed_at)
+    send(event, changed_at, user_id)
   end
 
   state_machine :state, initial: :preliminary do
     before_transition on: :mark_calulated, do: :calculate
+    after_transition on: :mark_calculated, do: :generate_tax_agency_report
     before_transition on: :mark_reported, do: :report
 
     event :mark_calculated do
@@ -39,14 +40,28 @@ class TaxReturn < ActiveRecord::Base
     end
   end
 
-  def calulate(transition)
+  def calculate(transition)
     # här skapas skatteunderlager
     self.calculated_at = transition.args[0]
+  end
+
+  def generate_tax_agency_report(transition)
+     create_tax_agency_transaction('tax', self.deadline, transition.args[1])
   end
 
   def report(transition)
     # här skapas verificate
     self.reported_at = transition.args[0]
+  end
+
+  def create_tax_agency_transaction(report_type, post_date, user_id)
+    tax_agency_transaction = TaxAgencyTransaction.new(
+          parent: self,
+          posting_date: post_date,
+          user_id: user_id,
+          report_type: report_type)
+    tax_agency_transaction.organization_id = organization_id
+    tax_agency_transaction.save
   end
 
   def can_calculate?
