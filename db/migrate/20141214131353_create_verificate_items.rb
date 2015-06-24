db86a528252e6c2e0779f2bfe425e43bc85b0bf1
mhn <max.hansson@gmail.com>
2015-06-24 12:55:52 +0200

    debit and credit should not be nil

--- db/migrate/20141214131353_create_verificate_items.rb
+++ db/migrate/20141214131353_create_verificate_items.rb
@@ -3,8 +3,8 @@ class CreateVerificateItems < ActiveRecord::Migration
     create_table :verificate_items do |t|
       t.integer  :account_id
       t.string   :description
-      t.decimal  :debit, precision: 11, scale: 2
-      t.decimal  :credit, precision: 11, scale: 2
+      t.decimal  :debit, precision: 11, scale: 2, default: 0.00
+      t.decimal  :credit, precision: 11, scale: 2, default: 0.00
       t.integer  :organization_id
       t.integer  :accounting_period_id
       t.integer  :verificate_id
