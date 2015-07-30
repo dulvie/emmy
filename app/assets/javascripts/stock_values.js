f8accf05ea175ad283f5ed9f9e5467cf42a5a9bd
mhn <max.hansson@gmail.com>
2015-07-30 08:38:52 +0200

    set max date for stock_values

--- app/assets/javascripts/stock_values.js
+++ app/assets/javascripts/stock_values.js
@@ -3,6 +3,7 @@ app.controller('stock_value_form_ctrl', function ($scope){
 	$scope.init = function() {
 		var v = $('#in_value_date').val().split(/\D/);
 		v.length == 1 ? $scope.v_date = new Date() : $scope.v_date = new Date(v[0], --v[1], v[2]);
+		$scope.max_date = new Date();
 	};
 
 	$scope.v_options = {'starting-day': 1,'show-weeks': false};
