b703f4883ce14af9f28c2b6309657603baa3590e
mhn <max.hansson@gmail.com>
2015-09-05 17:56:31 +0200

    show errormessage when default codes missing in accounts_payables and accounts_receivables

--- app/controllers/accounts_receivables_controller.rb
+++ app/controllers/accounts_receivables_controller.rb
@@ -5,14 +5,18 @@ class AccountsReceivablesController < ApplicationController
   def index
     @breadcrumbs = [["#{t(:accounts_receivables)}"]]
     default_code = current_organization.default_codes.find_by_code(03)
-    account = current_organization.accounts.where('accounting_plan_id = ? AND default_code_id = ?', @accounting_period.accounting_plan_id, default_code.id).first
-    @ledger_account = current_organization.ledger_accounts.where('account_id = ?', account.id).first
-    @accounts_receivables = current_organization.sales.prepared.not_paid.order(:due_date)
-                            .joins("LEFT OUTER JOIN verificates ON verificates.parent_type = 'Sale' AND verificates.parent_id = sales.id")
-                            .select("sales.*, verificates.state as verificate_state, verificates.id AS verificate_id")
-                            .page(params[:page]).decorate
-    if @accounts_receivables.size == 0
-      redirect_to helps_show_message_path(message: I18n.t(:accounts_receivables_missing))
+    if default_code.blank?
+      redirect_to helps_show_message_path(message: I18n.t(:default_codes_missing))
+    else
+      account = current_organization.accounts.where('accounting_plan_id = ? AND default_code_id = ?', @accounting_period.accounting_plan_id, default_code.id).first
+      @ledger_account = current_organization.ledger_accounts.where('account_id = ?', account.id).first
+      @accounts_receivables = current_organization.sales.prepared.not_paid.order(:due_date)
+                              .joins("LEFT OUTER JOIN verificates ON verificates.parent_type = 'Sale' AND verificates.parent_id = sales.id")
+                              .select("sales.*, verificates.state as verificate_state, verificates.id AS verificate_id")
+                              .page(params[:page]).decorate
+      if @accounts_receivables.size == 0
+        redirect_to helps_show_message_path(message: I18n.t(:accounts_receivables_missing))
+      end
     end
   end
 
