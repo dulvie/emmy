cffd79e11c95083a457f706ded9e87a2f34e5970
mhn <max.hansson@gmail.com>
2015-09-05 17:56:31 +0200

    show errormessage when default codes missing in accounts_payables and accounts_receivables

--- app/controllers/accounts_payables_controller.rb
+++ app/controllers/accounts_payables_controller.rb
@@ -5,18 +5,22 @@ class AccountsPayablesController < ApplicationController
   def index
     @breadcrumbs = [["#{t(:accounts_payables)}"]]
     default_code = current_organization.default_codes.find_by_code(04)
-    account = current_organization.accounts.where('accounting_plan_id = ? AND default_code_id = ?', @accounting_period.accounting_plan_id, default_code.id).first
-    @ledger_account = current_organization.ledger_accounts.where('account_id = ?', account.id).first
-    @accounts_payables = current_organization.purchases
-                           .not_paid.order('due_date NULLS LAST')
-                          .joins("LEFT OUTER JOIN verificates ON verificates.parent_type = 'Purchase' AND verificates.parent_id = purchases.id")
-                          .select("purchases.*, verificates.state as verificate_state, verificates.id AS verificate_id")
-                          .page(params[:page]).decorate
-    @account_payables_paid = current_organization.purchases.where("money_state = 'paid'")
-                          .joins("INNER JOIN verificates ON verificates.parent_type = 'Purchase' AND verificates.parent_id = purchases.id AND verificates.state = 'preliminary'")
-                          .select("purchases.*, verificates.state as verificate_state, verificates.id AS verificate_id").decorate
-    if @accounts_payables.size == 0
-      redirect_to helps_show_message_path(message: I18n.t(:accounts_payables_missing))
+    if default_code.blank?
+      redirect_to helps_show_message_path(message: I18n.t(:default_codes_missing))
+    else
+      account = current_organization.accounts.where('accounting_plan_id = ? AND default_code_id = ?', @accounting_period.accounting_plan_id, default_code.id).first
+      @ledger_account = current_organization.ledger_accounts.where('account_id = ?', account.id).first
+      @accounts_payables = current_organization.purchases
+                             .not_paid.order('due_date NULLS LAST')
+                            .joins("LEFT OUTER JOIN verificates ON verificates.parent_type = 'Purchase' AND verificates.parent_id = purchases.id")
+                            .select("purchases.*, verificates.state as verificate_state, verificates.id AS verificate_id")
+                            .page(params[:page]).decorate
+      @account_payables_paid = current_organization.purchases.where("money_state = 'paid'")
+                            .joins("INNER JOIN verificates ON verificates.parent_type = 'Purchase' AND verificates.parent_id = purchases.id AND verificates.state = 'preliminary'")
+                            .select("purchases.*, verificates.state as verificate_state, verificates.id AS verificate_id").decorate
+      if @accounts_payables.size == 0
+        redirect_to helps_show_message_path(message: I18n.t(:accounts_payables_missing))
+      end
     end
   end
 
