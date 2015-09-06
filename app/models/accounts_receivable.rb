2ddf18353246ef14d5a6a3711dada9561de73754
mhn <max.hansson@gmail.com>
2015-09-06 20:38:44 +0200

    check dependencies in model

--- app/models/accounts_receivable.rb
+++ app/models/accounts_receivable.rb
@@ -1,13 +1,40 @@
-class AccountReceivable
+class AccountsReceivable
   include ActiveModel::Validations
   include ActiveModel::Conversion
   extend ActiveModel::Naming
 
-  def initialize
+  @default_codes
+  @accounting_period
 
+  def initialize(accounting_period, default_codes)
+    @accounting_period = accounting_period
+    @default_codes = default_codes
   end
 
+  validate :dependence
 
+  def dependence
+    if @accounting_period.blank?
+      errors.add(:base, I18n.t(:accounting_period_missing))
+      return
+    end
+    if @default_codes.blank?
+      errors.add(:base, I18n.t(:default_codes_missing))
+      return
+    end
+    if @default_codes.find_by_code(03).blank?
+      errors.add(:base,  I18n.t(:default_code_missing))
+    end
+  end
+
+  def default_code_id
+    default_code = @default_codes.find_by_code(03)
+    return default_code.id
+  end
+
+  def accounting_plan_id
+    @accounting_period.accounting_plan_id
+  end
 
   def persisted?
     false
