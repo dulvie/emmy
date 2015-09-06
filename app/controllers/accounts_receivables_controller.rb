69644bca8f4914365628e0aa16c62fbd5fb3191a
mhn <max.hansson@gmail.com>
2015-09-06 20:38:44 +0200

    check dependencies in model

--- app/controllers/accounts_receivables_controller.rb
+++ app/controllers/accounts_receivables_controller.rb
@@ -1,32 +1,28 @@
 class AccountsReceivablesController < ApplicationController
   respond_to :html, :json, :pdf
-  before_filter :load_accounting_period
+  before_filter :load_dependence
 
   def index
     @breadcrumbs = [["#{t(:accounts_receivables)}"]]
-    default_code = current_organization.default_codes.find_by_code(03)
-    if default_code.blank?
-      redirect_to helps_show_message_path(message: I18n.t(:default_codes_missing))
-    else
-      account = current_organization.accounts.where('accounting_plan_id = ? AND default_code_id = ?', @accounting_period.accounting_plan_id, default_code.id).first
-      @ledger_account = current_organization.ledger_accounts.where('account_id = ?', account.id).first
-      @accounts_receivables = current_organization.sales.prepared.not_paid.order(:due_date)
-                              .joins("LEFT OUTER JOIN verificates ON verificates.parent_type = 'Sale' AND verificates.parent_id = sales.id")
-                              .select("sales.*, verificates.state as verificate_state, verificates.id AS verificate_id")
-                              .page(params[:page]).decorate
-      if @accounts_receivables.size == 0
-        redirect_to helps_show_message_path(message: I18n.t(:accounts_receivables_missing))
-      end
+    account = current_organization.accounts.where('accounting_plan_id = ? AND default_code_id = ?', @accounts_receivable.accounting_plan_id, @accounts_receivable.default_code_id).first
+    @ledger_account = current_organization.ledger_accounts.where('account_id = ?', account.id).first
+    @accounts_receivables = current_organization.sales.prepared.not_paid.order(:due_date)
+                            .joins("LEFT OUTER JOIN verificates ON verificates.parent_type = 'Sale' AND verificates.parent_id = sales.id")
+                            .select("sales.*, verificates.state as verificate_state, verificates.id AS verificate_id")
+                            .page(params[:page]).decorate
+    if @accounts_receivables.size == 0
+      redirect_to helps_show_message_path(message: I18n.t(:accounts_receivables_missing))
     end
   end
 
   private
 
-  def load_accounting_period
+  def load_dependence
+    @default_codes = current_organization.default_codes
     @accounting_period = current_organization.accounting_periods.last
-    unless @accounting_period
-      redirect_to helps_show_message_path(message: I18n.t(:accounting_period_missing))
+    @accounts_receivable = AccountsReceivable.new(@accounting_period, @default_codes)
+    unless @accounts_receivable.valid?
+      redirect_to helps_show_message_path(message: @accounts_receivable.errors[:base][0])
     end
-  end
-
+  end  
 end
