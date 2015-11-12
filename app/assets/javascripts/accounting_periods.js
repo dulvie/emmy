1b5f4586da7baeb77068b8515ab861a7184cbf40
Max Hansson <max.hansson@dulvie.se>
2015-11-12 18:49:30 +0100

    fieldinfo for accounitng_period

--- app/assets/javascripts/accounting_periods.js
+++ app/assets/javascripts/accounting_periods.js
@@ -1,4 +1,4 @@
-app.controller('accounting_period_form_ctrl', function ($scope) {
+app.controller('accounting_period_form_ctrl', function ($scope, $modal, $sce) {
 
     $scope.init = function() {
     	var from = $('#in_from').val().split(/\D/);
@@ -22,5 +22,36 @@ app.controller('accounting_period_form_ctrl', function ($scope) {
 		$event.stopPropagation();
 		$scope.to_open = true;
 	};
+
+    $scope.show_info = function($event, info) {
+        $scope.open_info('mb', 'infoContent', info);
+        $event.preventDefault();
+        $event.stopPropagation();
+    };
+
+    $scope.open_info = function (size, el, info) {
+        var info_el= '#'+info;
+        var info_html = $(info_el).html();
+        $scope.info = $sce.trustAsHtml(info_html);
+        var elem = '#'+el;
+        var temp = $(elem).html();
+        var modalInstance = $modal.open({
+            template: temp,
+            controller: 'ModalInfoInstanceCtrl',
+            size: size,
+            resolve: {
+                info: function () {
+                    return $scope.info;
+                }
+            }
+        });
+        modalInstance.result.then(function (selectedItem) {
+            $scope.selected = selectedItem;
+        }, function () {
+            //$log.info('Modal dismissed at: ' + new Date());
+        });
+        //$event.preventDefault();
+        //$event.stopPropagation();
+    };
 
 });
