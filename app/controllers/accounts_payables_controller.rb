fc21b49c9c62715bdab14d54eca6213a2951ae12
Max Hansson <max.hansson@dulvie.se>
2015-11-17 10:24:46 +0100

    rubocop style guides

--- app/controllers/accounts_payables_controller.rb
+++ app/controllers/accounts_payables_controller.rb
@@ -4,21 +4,22 @@ class AccountsPayablesController < ApplicationController
 
   def index
     @breadcrumbs = [["#{t(:accounts_payables)}"]]
-    account = current_organization.accounts.where('accounting_plan_id = ? AND default_code_id = ?', @accounts_payable.accounting_plan_id, @accounts_payable.default_code_id).first
+    account = current_organization.accounts
+                  .where('accounting_plan_id = ? AND default_code_id = ?', @accounts_payable.accounting_plan_id, @accounts_payable.default_code_id)
+                  .first
     @ledger_account = current_organization.ledger_accounts.where('account_id = ?', account.id).first
     @accounts_payables = current_organization.purchases
                            .not_paid.order('due_date NULLS LAST')
                           .joins("LEFT OUTER JOIN verificates ON verificates.parent_type = 'Purchase' AND verificates.parent_id = purchases.id")
-                          .select("purchases.*, verificates.state as verificate_state, verificates.id AS verificate_id")
+                          .select('purchases.*, verificates.state as verificate_state, verificates.id AS verificate_id')
                           .page(params[:page]).decorate
     @account_payables_paid = current_organization.purchases.where("money_state = 'paid'")
                           .joins("INNER JOIN verificates ON verificates.parent_type = 'Purchase' AND verificates.parent_id = purchases.id AND verificates.state = 'preliminary'")
-                          .select("purchases.*, verificates.state as verificate_state, verificates.id AS verificate_id").decorate
+                          .select('purchases.*, verificates.state as verificate_state, verificates.id AS verificate_id').decorate
     if @accounts_payables.size == 0
       redirect_to helps_show_message_path(message: "#{I18n.t(:accounts_payables)} #{I18n.t(:missing)}")
     end
   end
-
 
   private
 
