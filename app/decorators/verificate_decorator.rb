c8d7150f81290da9c8b2d9c8b2fc3ccc08c8f74b
Max Hansson <max.hansson@dulvie.se>
2015-11-20 11:16:01 +0100

    rubocop style guides

--- app/decorators/verificate_decorator.rb
+++ app/decorators/verificate_decorator.rb
@@ -3,8 +3,8 @@ class VerificateDecorator < Draper::Decorator
   delegate_all
 
   def format_posting_date
-    return l(object.posting_date, format: "%Y-%m-%d") if object.posting_date
-    ""
+    return l(object.posting_date, format: '%Y-%m-%d') if object.posting_date
+    ''
   end
 
   def parent
@@ -15,12 +15,13 @@ class VerificateDecorator < Draper::Decorator
     return  h.link_to I18n.t(:import_bank_file), h.import_bank_file_path(verificate.parent.import_bank_file) if object.parent_type == 'ImportBankFileRow' && verificate.parent
     return  h.link_to I18n.t(:purchase), h.purchase_path(object.parent_id) if object.parent_type == 'Purchase'
     return  h.link_to I18n.t(:stock_value), h.stock_value_path(object.parent_id) if object.parent_type == 'StockValue'
-    return ' '
+    ' '
   end
 
   def total_debit
     number_with_precision(object.total_debit, precision: 2)
   end
+
   def total_credit
     number_with_precision(object.total_credit, precision: 2)
   end
@@ -32,13 +33,13 @@ class VerificateDecorator < Draper::Decorator
   def pretty_state
     l = 'default'
     case object.state
-      when 'preliminary'
-        l = 'info'
-        str = h.t(:preliminary)
-      when 'final'
-        l = 'success'
-        str = h.t(:final)
-     end
+    when 'preliminary'
+      l = 'info'
+      str = h.t(:preliminary)
+    when 'final'
+      l = 'success'
+      str = h.t(:final)
+   end
     h.labelify(str, l)
   end
 end
--- app/decorators/wage_period_decorator.rb
+++ app/decorators/wage_period_decorator.rb
@@ -2,43 +2,45 @@ class WagePeriodDecorator < Draper::Decorator
   delegate_all
 
   def wage_from
-    return l(object.wage_from, format: "%Y-%m-%d") if object.wage_from
-    ""
+    return l(object.wage_from, format: '%Y-%m-%d') if object.wage_from
+    ''
   end
 
   def wage_to
-    return l(object.wage_to, format: "%Y-%m-%d") if object.wage_to
-    ""
+    return l(object.wage_to, format: '%Y-%m-%d') if object.wage_to
+    ''
   end
+
   def deadline
-    return l(object.deadline, format: "%Y-%m-%d") if object.deadline
-    ""
+    return l(object.deadline, format: '%Y-%m-%d') if object.deadline
+    ''
   end
+
   def state
     l = 'default'
     case object.state
-      when 'preliminary'
-        l = 'info'
-        str = h.t(:preliminary)
-      when 'wage_calculated'
-        l = 'warning'
-        str = h.t(:wage_calculated)
-      when 'wage_reported'
-        l = 'warning'
-        str = h.t(:wage_reported)
-      when 'wage_closed'
-        l = 'success'
-        str = h.t(:wage_closed)
-      when 'tax_calculated'
-        l = 'warning'
-        str = h.t(:tax_calculated)
-      when 'tax_reported'
-        l = 'warning'
-        str = h.t(:tax_reported)
-      when 'tax_closed'
-        l = 'success'
-        str = h.t(:tax_closed)
-     end
+    when 'preliminary'
+      l = 'info'
+      str = h.t(:preliminary)
+    when 'wage_calculated'
+      l = 'warning'
+      str = h.t(:wage_calculated)
+    when 'wage_reported'
+      l = 'warning'
+      str = h.t(:wage_reported)
+    when 'wage_closed'
+      l = 'success'
+      str = h.t(:wage_closed)
+    when 'tax_calculated'
+      l = 'warning'
+      str = h.t(:tax_calculated)
+    when 'tax_reported'
+      l = 'warning'
+      str = h.t(:tax_reported)
+    when 'tax_closed'
+      l = 'success'
+      str = h.t(:tax_closed)
+   end
     h.labelify(str, l)
   end
 end
