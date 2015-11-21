034e3a31e705c84bba6c25fb8c54e708615e643b
Max Hansson <max.hansson@dulvie.se>
2015-11-21 11:35:25 +0100

    rubocop style guides

--- app/decorators/account_decorator.rb
+++ app/decorators/account_decorator.rb
@@ -3,14 +3,14 @@ class AccountDecorator < Draper::Decorator
 
   def tax_code_code
     return object.tax_code.code if object.tax_code
-    ""
+    ''
   end
 
   def ink_code_code
     return object.ink_code.code if object.ink_code
-    ""
+    ''
   end
-  
+
   def ne_code_code
     return object.ne_code.code if object.ne_code
   end
