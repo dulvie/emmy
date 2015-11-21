1212f2bcb9e63d7e00413f2607943f988c6b2ed7
Max Hansson <max.hansson@dulvie.se>
2015-11-21 11:22:45 +0100

    rubocop style guides

--- app/decorators/ledger_transaction_decorator.rb
+++ app/decorators/ledger_transaction_decorator.rb
@@ -3,9 +3,10 @@ class LedgerTransactionDecorator < Draper::Decorator
   delegate_all
 
   def posting_date
-    return l(object.posting_date, format: "%Y-%m-%d") if object.posting_date
-    ""
+    return l(object.posting_date, format: '%Y-%m-%d') if object.posting_date
+    ''
   end
+
   def sum
     number_with_precision(object.sum, precision: 2)
   end
