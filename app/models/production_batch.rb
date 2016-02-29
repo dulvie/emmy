ed188bfb16635458ae515e8556b66d88f101583f
Max Hansson <max.hansson@dulvie.se>
2016-02-29 18:19:02 +0100

    move methods to decorator, close #168 and ref #196

--- app/models/production_batch.rb
+++ app/models/production_batch.rb
@@ -1,4 +1,5 @@
 class ProductionBatch
+  include Draper::Decoratable
   include ActiveModel::Model
 
   attr_accessor :organization_id, :production_id, :item_id, :name, :comment,
@@ -15,16 +16,6 @@ class ProductionBatch
     errors.add(:name, :taken) if u > 0
   end
 
-  def distributor_inc_vat
-    return 0 if !distributor_price || !vat
-    (distributor_price + distributor_price * vat.add_factor)/100
-  end
-
-  def retail_inc_vat
-    return 0 if !retail_price || !vat
-    (retail_price + retail_price * vat.add_factor)/100
-  end
-  
   def submit
     return false unless valid?
     @production = Production.find(production_id)
