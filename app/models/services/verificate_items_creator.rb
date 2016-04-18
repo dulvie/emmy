197bb312eac9bd0d3f8653891eaa79e4e7afe9ad
Max Hansson <max.hansson@dulvie.se>
2016-04-18 09:49:34 +0200

    handling decimals in verificate templates, close #221

--- app/models/services/verificate_items_creator.rb
+++ app/models/services/verificate_items_creator.rb
@@ -14,8 +14,8 @@ module Services
         verificate_item.accounting_period = @verificate.accounting_period
         verificate_item.account_id = item[1][:account_id]
         verificate_item.description = item[1][:description]
-        verificate_item.debit = item[1][:debit]
-        verificate_item.credit = item[1][:credit]
+        verificate_item.debit = item[1][:debit].gsub(",", ".")
+        verificate_item.credit = item[1][:credit].gsub(",", ".")
         verificate_item.save if item[1][:debit] != '0' || item[1][:credit] != '0'
       end
       if @verificate.balanced?
