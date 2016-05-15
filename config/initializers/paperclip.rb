41a6d459703bffae13b092ccd2c95e547a79e328
Max Hansson <max.hansson@dulvie.se>
2016-05-15 12:10:45 +0200

    Merge remote-tracking branch 'remotes/origin/master' into sie-export
    Conflicts:
    	app/models/ability.rb
    	app/models/organization.rb
    	app/views/shared/_sidebar.html.haml
    	config/locales/se.yml
    	config/routes.rb

---
+++ config/initializers/paperclip.rb
@@ -0,0 +1,10 @@
+# Allow ".SE" as an extension for files with the MIME type "text/plain".
+Paperclip.options[:content_type_mappings] = {
+    se: 'text/plain',
+    SE: 'text/plain',
+    csv: 'application/vnd.ms-excel'
+}
+# Do not trust the browser, it will send .se -files with content_type=application/octet-stream :(
+Paperclip::UploadedFileAdapter.content_type_detector = Paperclip::ContentTypeDetector
+
+puts "paper clip options: #{Paperclip.options[:content_type_mappings].inspect}"
