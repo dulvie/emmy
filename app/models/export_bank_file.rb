707901c1d118f826b4199af149b7ea8970978349
Max Hansson <max.hansson@dulvie.se>
2016-05-17 15:29:58 +0200

    sie_import ok in master

--- app/models/export_bank_file.rb
+++ app/models/export_bank_file.rb
@@ -6,39 +6,52 @@ class ExportBankFile < ActiveRecord::Base
   # t.string   :pay_account
   # t.string   :organization_number
   # t.string   :iban
+  # t.string   :state
+  # t.attachment :download
   # t.integer  :organization_id
   # t.integer  :user_id
   # t.timestamps
 
   attr_accessible :export_date, :from_date, :to_date, :reference, :pay_account,
-                  :organization_number, :iban
+                  :organization_number, :iban, :download
 
   belongs_to :organization
   belongs_to :user
   has_many   :export_bank_file_rows, dependent: :delete_all
-  # validates_attachment_content_type :upload, content_type: ['text/csv']
+  has_attached_file :download
 
+  validates_attachment_content_type :download, content_type: ['text/plain']
+
+
+  VALID_EVENTS = %w(export_event)
   TYPES = ['Fakturabetalning', 'Löneutbetalning']
 
-  after_commit :create_bank_transaction
+  after_commit :enqueue_export_event, on: :create
+
+  def enqueue_export_event
+    if completed?
+      logger.info "** ExportBankFile #{id} already completed, will not enqueue_event"
+      return
+    end
+    logger.info '** ExportBankFile enqueue a job that will create a export file.'
+    Resque.enqueue(Job::ExportBankFileEvent, id, 'export_event')
+  end
+
+  # Run from the 'Job::ExportBankFileEvent' model
+  def export_event
+    return nil if completed?
+    export_bank_file = Services::ExportBankFileCreator.new(self)
+    if export_bank_file.read_and_create_file
+      logger.info "** ExportBankFile #{id} read_and_create returned ok, marking complete"
+      complete
+    else
+      logger.info "** ImportBankFile #{id} parse/import did NOT return ok, not marking complete"
+    end
+  end
 
   def file_name
-    return "#{organization.slug}_payment_bank_file_#{id}.csv" if self.reference == TYPES[0]
-    return "#{organization.slug}_wage_bank_file_#{id}.csv" if self.reference == TYPES[1]
-    return nil
-  end
-
-  def directory
-    'tmp/downloads'
-  end
-
-  def file_exists?
-    File.exists?directory+'/'+file_name
-  end
-
-  def file_filter
-    return '/' + "#{organization.slug}_payment_bank_file*.*" if self.reference == TYPES[0]
-    return '/' + "#{organization.slug}_wage_bank_file*.*" if self.reference == TYPES[1]
+    return "#{organization.slug}_payment_bank_file_#{id}" if self.reference == TYPES[0]
+    return "#{organization.slug}_wage_bank_file_#{id}" if self.reference == TYPES[1]
     return nil
   end
 
@@ -46,15 +59,13 @@ class ExportBankFile < ActiveRecord::Base
     true
   end
 
-  def create_bank_transaction
-    @bank_file_trans = BankFileTransaction.new
-    @bank_file_trans.parent = self
-    @bank_file_trans.execute = 'export'
-    @bank_file_trans.complete = 'false'
-    @bank_file_trans.directory = directory
-    @bank_file_trans.file_name = file_name
-    @bank_file_trans.user = user
-    @bank_file_trans.organization = organization
-    @bank_file_trans.save
+  state_machine :state, initial: :created do
+    event :complete do
+      transition created: :completed
+    end
+  end
+
+  def completed?
+    state.eql? 'completed'
   end
 end
