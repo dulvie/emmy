8f5ff7a0e225a89dd1b7fdc26eb15c9cf2deb11e
Max Hansson <max.hansson@dulvie.se>
2016-06-15 19:22:24 +0200

    Merge branch 'master' into default_code_job
    # Conflicts:
    #	app/models/organization.rb
    #	db/schema.rb

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -25,17 +25,16 @@ class Organization < ActiveRecord::Base
   has_many :users, through: :organization_roles
 
   [:accounting_classes, :accounting_groups, :accounting_periods, :accounting_plans, :accounts,
-   :balance_transactions, :bank_file_transactions, :batches, :batch_transactions, :closing_balances,
-   :comments, :contact_relations, :contacts, :conversions, :customers, :default_codes,
-   :default_code_headers, :documents, :employees, :export_bank_files, :export_bank_file_rows, :imports,
-   :ink_codes, :ink_code_headers, :import_bank_files, :import_bank_file_rows, :inventories, :items,
-   :ledgers, :ledger_accounts, :ledger_transactions, :manuals, :materials, :ne_codes, :ne_code_headers,
-   :opening_balances, :opening_balance_items, :productions, :purchases, :purchase_items, :result_units,
-   :shelves, :sales, :sale_items, :sie_exports, :sie_imports, :stock_values, :sie_transactions,
-   :stock_value_items, :suppliers, :table_transactions, :tax_codes, :tax_code_headers, :tax_returns,
+   :bank_file_transactions, :batches, :batch_transactions, :closing_balances,
+   :code_transactions, :comments, :contact_relations, :contacts, :conversions, :customers, :default_codes,
+   :documents, :employees, :export_bank_files, :export_bank_file_rows, :imports, :ink_codes, :import_bank_files,
+   :import_bank_file_rows, :inventories, :items, :ledgers, :ledger_accounts, :ledger_transactions,
+   :manuals, :materials, :ne_codes, :opening_balances, :opening_balance_items, :productions, :purchases,
+   :purchase_items, :result_units, :shelves, :sales, :sale_items, :sie_exports, :sie_imports, :stock_values,
+   :sie_transactions, :stock_value_items, :suppliers, :table_transactions, :tax_codes, :tax_returns,
    :tax_return_reports, :tax_tables, :tax_table_rows, :templates, :template_items, :transfers, :units,
-   :vat_periods, :vat_reports, :vats, :verificates, :verificate_items, :wage_periods, :wage_reports,
-   :wages, :warehouses].each do |model_sym|
+   :vat_periods, :vat_reports, :vats, :verificates, :verificate_items, :wage_periods, :wage_reports, :wages,
+   :warehouses].each do |model_sym|
     has_many model_sym
   end
 
