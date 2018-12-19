9f47a09eba0d519fb61be12a94b5ea0b49f51f75
Max Hansson <max.hansson@dulvie.se>
2018-12-19 10:38:10 +0100

    Backgroudjob for tax_table moved to ActiveJob

---
+++ app/jobs/tax_table_job.rb
@@ -0,0 +1,15 @@
+  class TaxTableJob < ApplicationJob
+    @queue = :tax_table_jobs
+    def perform(tax_table_id, job_name)
+      tax_table = TaxTable.find(tax_table_id)
+      if TaxTable::VALID_EVENTS.include?(job_name)
+        Rails.logger.info "will execute #{job_name} on #{tax_table.id}"
+        tax_table.send(job_name)
+      else
+        Rails.logger.info "** Job::TaxTableJob unknown event_name(#{job_name})"
+      end
+    rescue ActiveRecord::RecordNotFound
+      Rails.logger.info '** Job::TaxTableJob could not find ' \
+        "object with id #{tax_table_id}"
+    end
+  end
