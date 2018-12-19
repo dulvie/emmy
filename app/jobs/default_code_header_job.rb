03e22bb032b3c0e6901f885f62a9cb2f6e54ce49
Max Hansson <max.hansson@dulvie.se>
2018-12-19 16:05:01 +0100

    efault_code_header background job moved to ActiveJob

---
+++ app/jobs/default_code_header_job.rb
@@ -0,0 +1,15 @@
+class DefaultCodeHeaderJob < ApplicationJob
+  @queue = :default_code_header_jobs
+  def perform(default_code_header_id, import_job)
+    default_code_header = DefaultCodeHeader.find(default_code_header_id)
+    if DefaultCodeHeader::VALID_JOBS.include?(import_job)
+      Rails.logger.info "will execute #{import_job} on #{default_code_header.id}"
+      default_code_header.send(import_job)
+    else
+      Rails.logger.info "** Job::DefaultCodeHeaderJob unknown job(#{import_job})"
+    end
+  rescue ActiveRecord::RecordNotFound
+    Rails.logger.info '** Job::DefaultCodeHeaderJob could not find ' \
+      "object with id #{default_code_header_id}"
+  end
+end
