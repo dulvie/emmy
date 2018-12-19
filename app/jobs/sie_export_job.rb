6ad636fad9bb158035a4c3a5210f140de170adad
Max Hansson <max.hansson@dulvie.se>
2018-12-19 20:59:38 +0100

    sie_export/import backgrounds job moved to ActiveJob

---
+++ app/jobs/sie_export_job.rb
@@ -0,0 +1,15 @@
+class SieExportJob < ApplicationJob
+  @queue = :export_sie_jobs
+  def perform(sie_export_id, job_name)
+    sie_export = SieExport.find(sie_export_id)
+    if SieExport::VALID_JOBS.include?(job_name)
+      Rails.logger.info "will execute #{job_name} on #{sie_export.id}"
+      sie_export.send(job_name)
+    else
+      Rails.logger.info "** SieExportJob unknown job_name(#{job_name})"
+    end
+  rescue ActiveRecord::RecordNotFound
+    Rails.logger.info '** SieExportJob could not find ' \
+      "object with id #{sie_export_id}"
+  end
+end
