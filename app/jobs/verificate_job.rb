ebef5075886eb62569c32e935530f455c646cce0
Max Hansson <max.hansson@dulvie.se>
2018-12-19 12:15:53 +0100

    verificate backgroudjob moved to ActiveJob

---
+++ app/jobs/verificate_job.rb
@@ -0,0 +1,15 @@
+class VerificateJob < ApplicationJob
+  @queue = :verificate_jobs
+  def perform(verificate_id, job_name)
+    verificate = Verificate.find(verificate_id)
+    if Verificate::VALID_EVENTS.include?(job_name)
+      Rails.logger.info "will execute #{job_name} on #{verificate.id}"
+      verificate.send(job_name)
+    else
+      Rails.logger.info "** Job::VerificateJob unknown job_name(#{job_name})"
+    end
+  rescue ActiveRecord::RecordNotFound
+    Rails.logger.info '** Job::VerificateJob could not find ' \
+      "object with id #{verificate_id}"
+  end
+end
