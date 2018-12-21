0f434e361e5d6735a81baccb84b3e6710791e494
Max Hansson <max.hansson@dulvie.se>
2018-12-21 11:00:56 +0100

    batch_transaction backgroundjob moved to ActiveJob

--- app/jobs/batch_transaction_job.rb
+++ app/jobs/batch_transaction_job.rb
@@ -1,7 +1,7 @@
-class Job::BatchTransactionEvent
-  @queue = :batch_transaction_events
+class BatchTransactionJob < ApplicationJob
+  queue_as :batch_transaction_jobs
 
-  def self.perform(batch_transaction_id)
+  def perform(batch_transaction_id)
     Rails.logger.info "BatchTransactionEvent.perform(#{batch_transaction_id})"
     t = BatchTransaction.find(batch_transaction_id)
     recalculate_shelf(t)
@@ -9,7 +9,7 @@ class Job::BatchTransactionEvent
 
   # Recalculate (cache) the quantity of how many batches a warehouse currently has.
   # A new shelf is created if there aren't already one for this warehouse/batch combination.
-  def self.recalculate_shelf(t)
+  def recalculate_shelf(t)
     shelves = t.warehouse.shelves.where(batch_id: t.batch_id)
     unless shelves.size > 0
       shelf = Shelf.new(warehouse_id: t.warehouse_id, batch_id: t.batch_id)
