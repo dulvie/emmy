2b5f1b7b1dc5abb36e9e3b489e1609250be34a2f
joel hansson <joel.hansson@dulvie.se>
2019-03-31 22:35:50 +0200

    refactor invoice_email contacts query

--- app/mailers/invoice_mailer.rb
+++ app/mailers/invoice_mailer.rb
@@ -10,7 +10,7 @@ class InvoiceMailer < ActionMailer::Base
     @organization = @sale.organization
     @subject = "#{t(:invoice_from)} #{@organization.name}"
     @subject = mail_template.subject if mail_template
-    @text = set_text(@organization, @sale, mail_template) if mail_template
+    @text = set_text(@sale, mail_template) if mail_template
     attachments["#{@sale.invoice_number}.pdf"] = File.read(@sale.document.upload.path)
     mail(
       from: @organization.email,
@@ -24,7 +24,7 @@ class InvoiceMailer < ActionMailer::Base
     @organization = @sale.organization
     @subject = "#{t(:reminder_from)} #{@organization.name}"
     @subject = mail_template.subject if mail_template
-    @text = set_text(@organization, @sale, mail_template) if mail_template
+    @text = set_text(@sale, mail_template) if mail_template
     attachments["#{@sale.invoice_number}.pdf"] = File.read(@sale.document.upload.path)
     mail(
         from: @organization.email,
@@ -34,22 +34,31 @@ class InvoiceMailer < ActionMailer::Base
     )
   end
 
-  def set_text(organization, sale, mail_template)
-    if !mail_template.text.include? "#"
-      return mail_template.text
-    elsif sale.user.contacts.present?
-      sale_fields = sale.attributes
-      contact_fields = organization.contacts.find(sale.user.contacts.first.id).attributes
-      fields = sale_fields.merge(contact_fields)
-      return set_variables(mail_template.text, fields)
-    end
-    nil
+  def set_text(sale, mail_template)
+    return mail_template.text unless mail_template.text.include? '#'
+
+    fields = sale.attributes.merge(contact_attributes(sale))
+    set_variables(mail_template.text, fields)
   end
 
   def set_variables(text, fields)
-    ALLOWED_COLUMNS.each do | k, v |
-      text.gsub! k, fields[v]
+    ALLOWED_COLUMNS.each do |k, v|
+      text.gsub!(k, fields[v])
     end
     return text
+  end
+
+  def contact_attributes(sale)
+    user_contact = sale.user.contacts.search_by_org(sale.organization).first
+
+    return user_contact.attributes if user_contact
+
+    logger.info '--> invoice_mailer: ' \
+                "No contacts found for #{sale.user.email} faling back to user info"
+    return {
+      'name' => sale.user.name,
+      'email' => sale.user.email,
+      'telephone' => ' - ' # there is no telephone on the user account
+    }
   end
 end
